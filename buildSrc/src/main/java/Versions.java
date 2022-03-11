91dce24cbc9e37ce376b4dfde68558c294b792c8
sergey <petrelevich@yandex.ru>
2022-03-11 20:23:07 +0300

    L16 io

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,6 +1,8 @@
 public interface Versions {
-    String guava = "31.0.1-jre";
+    String guava = "31.1-jre";
     String jmh = "1.33";
     String jol = "0.16";
     String asm = "9.2";
+    String glassfishJson = "1.1.4";
+    String protobuf = "3.19.4";
 }
