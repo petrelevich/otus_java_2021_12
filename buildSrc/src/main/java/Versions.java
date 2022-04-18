e3df48424568663fc15acdfe91f08e22feb530c2
sergey <petrelevich@yandex.ru>
2022-04-18 23:22:47 +0300

    L27 websocket

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -22,4 +22,9 @@ public interface Versions {
 
     String reflections = "0.10.2";
 
+    String sockjs = "1.5.1";
+    String stomp = "2.3.4";
+    String bootstrap = "5.1.3";
+    String springDocOpenapiUi = "1.6.7";
+    String jsr305 = "3.0.2";
 }
