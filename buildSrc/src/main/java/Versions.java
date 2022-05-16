81c0fb33b02801accaf90d5ce31c40e77697ff10
stvort <stvort@mail.ru>
2022-05-17 03:08:55 +0400

    L33-multiprocess added

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -27,4 +27,9 @@ public interface Versions {
     String bootstrap = "5.1.3";
     String springDocOpenapiUi = "1.6.7";
     String jsr305 = "3.0.2";
+
+    String grpc = "1.44.1";
+    String errorProneAnnotations = "2.11.0";
+    String tomcatAnnotationsApi = "6.0.53";
+
 }
